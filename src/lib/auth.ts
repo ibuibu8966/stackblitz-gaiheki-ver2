4799a3df988cf2692403b7ecd5c34cba65ffcdb2
ibuibu8966 <ibuki.8966@gmail.com>
2025-03-31 16:33:57 +0900

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,3 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { prisma } from "./prisma";
